6e6bd662c3138b79162ecffca7f1477ea8178b16
Navnish Singh PfSRUHwNNR <navnishlnb@gmail.com>
2023-01-19 17:58:06 +0000

    Add better comments

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,16 @@
-// Reusable function to add and remove event listeners
+/**
+ * Function to add and remove events in a dom element.
+ * @param {object} eventObject - Object container event as key and handler as its value
+ * @param {*} target - DOM element to add or remove event.
+ * @param {boolean} removeListener - To remove the event.
+ * @param {object} eventOptions - Options params for eventAction.
+ *
+ */
 export const manageEvent = ({
   eventObject,
   target,
   removeListener = false,
-  eventOptions = []
+  eventOptions = [],
 }) => {
   let eventAction = removeListener ? "removeEventListener" : "addEventListener";
 
@@ -12,7 +19,18 @@ export const manageEvent = ({
   });
 };
 
-export const manageClass = (element, className, remove = false) => {
-  const action = remove ? "remove" : "add";
-  element.classList[action](className);
-};
+/**
+ * Function to add class to a DOM element.
+ * @param {*} element - DOM element to add the className to.
+ * @param {string} className - Class name to add to the element.
+ */
+export const addClass = (element, className) =>
+  element.classList.add(className);
+
+/**
+ * Function to remove class to a DOM element.
+ * @param {*} element - DOM element to remove the className from.
+ * @param {string} className - Class name to remove from the element.
+ */
+export const removeClass = (element, className) =>
+  element.classList.remove(className);
